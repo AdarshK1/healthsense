2938b026a02c492d8932f1671368471b1844c03f
Adarsh <akulkarn@seas.upenn.edu>
2018-03-23 18:32:22 -0400

    "initial commit

--- HealthSense/ViewController.swift
+++ HealthSense/ViewController.swift
@@ -8,16 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ESTTriggerManagerDelegate  {
 
+    // 2. Add the trigger manager
+    let triggerManager = ESTTriggerManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 3. Set the trigger manager's delegate
+        self.triggerManager.delegate = self
+        
+        // add this below:
+        let rule1 = ESTOrientationRule.orientationEquals(
+            .horizontalUpsideDown, for: .door)
+        let rule2 = ESTMotionRule.motionStateEquals(
+            true, forNearableIdentifier: "7d516d6786eadf98")
+        let trigger = ESTTrigger(rules: [rule1, rule2], identifier: "tom the trigger")
+        
+        self.triggerManager.startMonitoring(for: trigger)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func triggerManager(_ manager: ESTTriggerManager,
+                        triggerChangedState trigger: ESTTrigger) {
+        if (trigger.identifier == "tom the trigger" && trigger.state == true) {
+            print("Hello, digital world! The physical world has spoken.")
+        } else {
+            print("Goodnight. <spoken in the voice of a turret from Portal>")
+        }
     }
 
 
